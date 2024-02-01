93117e60f47327aa26056e91d80f413e6b61e297
Akshit <akshitgaur2003@gmail.com>
2024-02-01 22:00:43 +0530

    Add Netflix clone files

---
+++ netlix_clone/src/Requests.js
@@ -0,0 +1,15 @@
+const key = 'd435290211b8c80a73775764b4dbcf1f'
+
+const requests = {
+    requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+    requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+    requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
+    requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=horror&page=1&include_adult=false`,
+
+    //https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=horror&page=1&include_adult=false
+
+
+    requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+}
+
+export default requests
